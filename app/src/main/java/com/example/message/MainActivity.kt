1defaa2db72be0db8d741775c7866fb718660a56
unknown <appstutorialesy@gmail.comm>
2021-10-01 14:38:31 -0600

    First commit

---
+++ app/src/main/java/com/example/message/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.message
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var editText: EditText
+    lateinit var button: Button
+    lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+        button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
+
+        button.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View?) {
+        val name = editText.text
+        textView.text = "Hello $name"
+    }
+}
